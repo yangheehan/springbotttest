982b7ccf65a9d4d170540561c48e4c4ff3eadb3d
yangheehan <hyh740901@hanmail.net>
2018-05-25 16:49:41 +0900

    springboot test

--- src/main/java/myspringboot/hero/HeroController.java
+++ src/main/java/myspringboot/hero/HeroController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,11 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//@CrossOrigin(origins = "http://localhost:4200") 
+@CrossOrigin(origins = "http://localhost:4200") 
 @RestController 
 @RequestMapping(value = "/heroes") 
 public class HeroController {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	private List<Hero> users = new ArrayList<Hero>();
 	
 	public HeroController() {
@@ -35,15 +39,18 @@ public class HeroController {
 	}
 	
 	@RequestMapping(method = RequestMethod.GET) 
-	public List<Hero> getUsers() 	{ 
+	public List<Hero> getUsers() 	{
+		logger.debug("목록조회");
 		return this.users; 
 	}
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET) 
 	public Hero getUser(@PathVariable("id") Long id) { 
+		logger.debug("상세조회:"+id);
 		return this.users.stream().filter(user -> user.getId() == id).findFirst().orElse(null); 
 	}
 	@RequestMapping(method = RequestMethod.POST) 
 	public Hero saveUser(@RequestBody Hero user) { 
+		logger.debug("등록:"+user);
 		Long nextId = 0L; 
 		if (this.users.size() != 0) { 
 			Hero lastUser = this.users.stream() .skip(this.users.size() - 1).findFirst().orElse(null); 
@@ -51,20 +58,24 @@ public class HeroController {
 		} 
 		user.setId(nextId); 
 		this.users.add(user); 
+		logger.debug("등록 후:"+user);
 		return user; 
 	}
 	@RequestMapping(method = RequestMethod.PUT) 
 	public Hero updateUser(@RequestBody Hero user) { 
+		logger.debug(" update 전 :"+user);
 		Hero modifiedUser = this.users.stream() .filter(u -> u.getId() == user.getId()).findFirst().orElse(null); 
 		modifiedUser.setName(user.getName()); 
 		modifiedUser.setId(user.getId());
-		
+		logger.debug(" update 후 :"+modifiedUser);
 		return modifiedUser; 
 	}
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE) 
 	public boolean deleteUser(@PathVariable Long id) { 
+		logger.debug("삭제 전:"+id);
 		Hero deleteUser = this.users.stream() .filter(user -> user.getId() == id).findFirst().orElse(null); 
 		if (deleteUser != null) { 
+			logger.debug("삭제된 hero:"+deleteUser);
 			this.users.remove(deleteUser); 
 			return true; 
 		} else  { return false; } 
@@ -74,6 +85,7 @@ public class HeroController {
 	//search
 	@RequestMapping(value = "/name/{name}", method = RequestMethod.GET) 
 	public List<Hero> searchHeroes(@PathVariable String name){
+		logger.debug("검색:"+name);
 		 return this.users.stream().filter(hero -> hero.getName().contains(name)).collect(Collectors.toList());
 	}
 		
